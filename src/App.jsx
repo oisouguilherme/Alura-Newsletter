00deb99880b1adb4198b19ad41eb8986eb3f4b80
oisouguilherme <gh.dourado77@gmail.com>
2023-01-03 16:31:29 -0300

    Finalizando projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { useState } from "react"
 import ArticleList from "./components/ArticleList/ArticleList"
+import Form from "./components/Form/Form"
 import Header from "./components/Header"
 
 const App = ()=>{
+  const [user, setUser] = useState()
+  const hasUser = Boolean(user)
   return(
-    <div className="">
-      <Header />
-      <ArticleList />
+    <div className="h-screen">
+      <Header user={user}/>
+      { hasUser && <ArticleList />}
+      { hasUser || <Form onSubmit={setUser} />}
     </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,10 @@
-import { MoonIcon } from '@heroicons/react/solid'
 import TogleTheme from './TogleTheme/TogleTheme'
 
-const Header = ()=> {
+const Header = ({user})=> {
     return(
         <div className="flex h-20 bg-alura-200 dark:bg-dark-200 justify-between items-center px-5 sm:rounded-xl sm:m-5 ">
-            <span className="text-gray-100">Olá, Usuário</span>
-            <h1 className="text-gray-100 text-xl">Alura Newsletter</h1>
+            <span className="text-gray-100">Olá, {user?.name || "Usuário"}</span>
+            <h1 className="text-gray-100 sm:hover:text-2xl sm:cursor-pointer text-xl">Alura Newsletter</h1>
             <TogleTheme />
         </div>
     )
